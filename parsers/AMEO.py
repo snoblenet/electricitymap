75fcf468a7ac00d57a5bc53ca6a689a9740284c9
Steven Noble <steven@snoblenet.local>
2017-04-13 06:25:53 +1000

    Add initial dictionaries for Australia

---
+++ parsers/AMEO.py
@@ -0,0 +1,34 @@
+AMEO_CATEGORY_DICTIONARY = {
+    'Bagasse': 'Biomass',
+    'Black Coal': 'Coal',
+    'Brown Coal': 'Coal',
+    'Coal Seam Methane': 'Gas',
+    'Diesel': 'Oil',
+    'Macadamia Nut Shells': 'Biomass',
+    'Hydro': 'Hydro',
+    'Kerosene': 'Oil',
+    'Landfill / Biogass': 'Biomass',
+    'Landfill Gas': 'Biomass',
+    'Landfill Methane / Landfill Gas': 'Biomass',
+    'Landfill, Biogas': 'Biomass',
+    'Macadamia Nut Shells': 'Biomass',
+    'Natural Gas': 'Gas',
+    'Natural Gas / Diesel': 'Gas',
+    'Natural Gas / Fuel Oil': 'Gas',
+    'Sewerage/Waste Water': 'Biomass',
+    'Solar': 'Solar',
+    'Solar PV': 'Solar',
+    'Waste Coal Mine Gas': 'Gas',
+    'Waste Water / Sewerage': 'Biomass',
+    'Water': 'Hydro',
+    'Wind': 'Wind'
+}
+
+AMEO_STATION_DICTIONARY = {
+    'Basslink HVDC Link': 'Import / Export',
+    'Bendeela / Kangaroo Valley Pumps': 'Storage',
+    'Rocky Point Cogeneration Plant': 'Storage',
+    'Wivenhoe Power Station No. 1 Pump': 'Storage',
+    'Wivenhoe Power Station No. 2 Pump': 'Storage',
+    'Yarwun Power Station': 'Coal'
+}
